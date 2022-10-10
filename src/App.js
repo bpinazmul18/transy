ab908905a4d6f06831f975e20911578a790a8986
Md Nazmul Haque <bpinazmul18@gmail.com>
2022-10-10 01:32:40 +0000

    Add styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Field from "./components/field";
 import Translate from "./components/translate";
 import Languages from "./components/languages";
 
+import "./styles.css";
+
 export default function App() {
   const [language, setLanguage] = useState("es");
   const [text, setText] = useState("");
